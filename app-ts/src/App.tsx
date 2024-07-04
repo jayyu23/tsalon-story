29bdef5a2c8ef8a88479593addec25b075eb03ad
jayyu <jayyu2011@hotmail.com>
2024-07-04 17:06:24 +0200

    integration with rainbowconnect

--- app-ts/src/App.tsx
+++ app-ts/src/App.tsx
@@ -3,15 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
+import { Web3ContextProvider } from './hooks/Web3ContextProvider';
 
 class App extends Component {
   render() {
     return (
+      <Web3ContextProvider>
       <BrowserRouter>
         <Routes>
           <Route path="/"  element={<Home />}/>
         </Routes>
       </BrowserRouter>
+      </Web3ContextProvider>
     );
   }
 }
