ebfd93a3dc7303749c43f2c3a7a5a19a0e569156
jayyu <jayyu2011@hotmail.com>
2024-08-01 23:49:42 -0400

    cleanup nologin

--- app-ts/src/components/Nologin.tsx
+++ app-ts/src/components/Nologin.tsx
@@ -9,40 +9,13 @@ const ErrorNoLogin: React.FC = () => {
   useEffect(() => {
     const timer = setTimeout(() => {
       navigate("/");
-    }, 10);
+    }, 1);
 
     return () => clearTimeout(timer); // Cleanup the timer on component unmount
   }, [navigate]);
 
   return (
-    <div className="my-0 py-0">
-      <NavBar />
-      <div
-        className="bg img-fluid"
-        style={{
-          backgroundImage: `url(${imgUrl})`,
-          height: "100vh",
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-        }}
-      >
-        <div className="container d-flex align-items-center justify-content-center text-center h-100">
-          <div className="text-white">
-            <h1 className="h1 mb-3" style={{ fontSize: 80 }}>
-              Error - Not Logged In
-            </h1>
-            <h5 className="mb-4">You must be logged in to view this data</h5>
-            <a
-              className="btn btn-primary my-3"
-              href="/"
-              style={{ borderRadius: 25 }}
-            >
-              Return Home
-            </a>
-          </div>
-        </div>
-      </div>
-    </div>
+    <div/>
   );
 };
 
